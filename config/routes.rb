1141d7a159f2760874b2c63e1449638560475651
EC2 Default User <ec2-user@ip-172-31-8-47.ap-northeast-1.compute.internal>
2023-01-29 09:56:50 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
   }
   root :to =>"homes#top"
     
