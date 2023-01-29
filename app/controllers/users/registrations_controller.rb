c69f733958dc8e22cd3fe05f3d21a24b07710614
EC2 Default User <ec2-user@ip-172-31-8-47.ap-northeast-1.compute.internal>
2023-01-29 09:28:19 +0000

    Initial Commit

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,6 +0,0 @@
-# - before_action :ensure_normal_user, only: :destroy
-# + before_action :ensure_normal_user, only: %i[update destroy]
-
-
-# - redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
-# + redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
